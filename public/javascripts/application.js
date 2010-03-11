d1d502f7c7780cd8d7f166211881362f0d51fc7d
David Workman <workmad3@gmail.com>
2010-03-11 21:22:05 +0000

    Drag and drop document position editing done on a single page basis

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -23,6 +23,25 @@ $(document).ready(function() {
         ui : {
             theme_name : "classic",
             theme_path : "/themes/classic/style.css"
+        },
+        callback : {
+            onmove : function(node, ref_node, type, tree, rb) {
+                var position_field = $("#documentation_page_position")[0];
+                var parent_select = $("#documentation_page_parent_id");
+                var previous_item = node.previousElementSibling;
+                var new_parent = node.parentNode.parentNode;
+                var new_parent_id = "";
+                var position = 1;
+                if (previous_item) {
+                    position = parseInt(previous_item.getAttribute("data-position")) + 1;
+                }
+                position_field.value = position;
+                if (new_parent.nodeName == "LI") {
+                    new_parent_id = new_parent.getAttribute("data-id");
+                }
+                parent_select.selectOptions(new_parent_id);
+
+            }
         }
     });
     $('fieldset.positioning').hide();
