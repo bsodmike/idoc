25dda62969b7ad97036e2de8be7538d04091b540
David Workman <workmad3@gmail.com>
2010-04-22 12:32:45 +0100

    Added in the JS tree editor required to make the tree editing usable. Also added in a link to the edit tree page on the side bar

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -48,6 +48,53 @@ $(document).ready(function() {
 });
 
 $(document).ready(function() {
+    $('#tree_editor').tree({
+        types : {
+            "default" : {
+                draggable : false
+            },
+            "editable" : {
+                draggable : true
+            }
+        },
+        ui : {
+            theme_name : "classic",
+            theme_path : "/themes/classic/style.css"
+        },
+        callback : {
+            onmove : function(node, ref_node, type, tree, rb) {
+                var node_name = node.getAttribute("data-name");
+                var position_field = $("#documentation_tree_documentation_page_" + node_name + "_position")[0];
+                var parent_select = $("#documentation_tree_documentation_page_" + node_name + "_parent_id");
+                var previous_item = node.previousElementSibling;
+                var next_item = node.nextElementSibling;
+                var new_parent = node.parentNode.parentNode;
+                var new_parent_id = "";
+                var position = 1;
+                if (previous_item) {
+                    position = parseInt(previous_item.getAttribute("data-position")) + 1;
+                }
+                var next_position = position + 1
+                while (next_item) {
+                  var next_position_field = $("#documentation_tree_documentation_page_" + next_item.getAttribute("data-name") + "_position")[0];
+                  next_position_field.value = next_position;
+                  next_position = next_position + 1;
+                  next_item = next_item.nextElementSibling;
+                }
+                position_field.value = position;
+                if (new_parent.nodeName == "LI") {
+                    new_parent_id = new_parent.getAttribute("data-id");
+                }
+                parent_select.selectOptions(new_parent_id);
+
+            }
+        }
+    });
+    $('#html_editor').hide();
+    $('#tree_editor').show();
+});
+
+$(document).ready(function() {
     var form = $('#header_login');
     var button = $('#header_login_button');
     form.hide();
