ee9301f133ae74bf6cee1719b31cca12bdcfae34
David Workman <workmad3@gmail.com>
2010-04-22 16:53:17 +0100

    Moving bundler setup into boot rather than preinitializer to attempt to get passenger working in smart spawn mode

--- config/boot.rb
+++ config/boot.rb
@@ -3,6 +3,22 @@
 
 RAILS_ROOT = "#{File.dirname(__FILE__)}/.." unless defined?(RAILS_ROOT)
 
+begin
+  require "rubygems"
+  require "bundler"
+
+  if Gem::Version.new(Bundler::VERSION) <= Gem::Version.new("0.9.5")
+    raise RuntimeError, "Your bundler version is too old." +
+     "Run `gem install bundler` to upgrade."
+  end
+
+  # Set up load paths for all bundled gems
+  Bundler.setup
+rescue Bundler::GemNotFound
+  raise RuntimeError, "Bundler couldn't find some gems." +
+    "Did you run `bundle install`?"
+end
+
 module Rails
   class << self
     def boot!
